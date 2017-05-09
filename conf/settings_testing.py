fb58dbd7c9e9eb60ee1b800e0a7e938bb7ca8a27
unknown <7303430@qq.com>
2017-05-09 17:04:43 +0800

    commit test

---
+++ conf/settings_testing.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+用于测试环境的全局配置
+"""
+import os
+from settings import BASE_DIR
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR,'db.sqlite3'),
+    }
+}
---
+++ home_application/views.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from common.mymako import render_mako_context
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse('hello world')
+
+def home(request):
+    """
+    首页
+    """
+    return render_mako_context(request, '/home_application/home.html')
+
+
+def dev_guide(request):
+    """
+    开发指引
+    """
+    return render_mako_context(request, '/home_application/dev_guide.html')
+
+
+def contactus(request):
+    """
+    联系我们
+    """
+    return render_mako_context(request, '/home_application/contact.html')
